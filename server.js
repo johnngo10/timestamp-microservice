7b49647d968505992e632f29784ee4e03d7d39dc
xuanngo10 <xuan.ngo10@gmail.com>
2020-11-23 22:04:13 -0800

    added timestamp route & unix, Utc functionality

--- server.js
+++ server.js
@@ -24,9 +24,33 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/timestamp/:date", function(req, res) {
+  let date = req.params.date;
+  let regDate = /^\d{4}-\d{2}-\d{2}$/;
+  let regUnix = /^[0-9]*$/;
+  let unix;
+  let utc;
+
+  if (date.match(regUnix)) {
+    const parseDate = parseInt(date);
+    utc = new Date(parseDate).toUTCString();
+    res.send({unix: date, utc: utc})
+  } else if (!date.match(regDate)) {
+    res.send({error: "Invalid Date"});
+  } else {
+    unix = new Date(date).getTime();
+    utc = new Date(unix).toUTCString();
+    res.send({unix: unix, utc: utc});
+  }
+})
 
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+
+// listen for requests
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// var listener = app.listen(process.env.PORT, function () {
+//   console.log('Your app is listening on port ' + listener.address().port);
+// });
